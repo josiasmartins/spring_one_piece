5ba2daed4a9835cdee165da39dd94ea759c07608
josias.m.caitano <josias.m.caitano@accenture.com>
2024-09-13 13:48:43 -0300

    feature: initial project

---
+++ src/test/java/com/josiasmartins/one_piece/OnePieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiasmartins.one_piece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
